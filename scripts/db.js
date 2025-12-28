14161cbcaf74c031e3267486a0f22655a969031b
Alexander Ruhl <ruhl-alex@web.de>
2025-12-28 22:53:24 +0100

    Add database

---
+++ scripts/db.js
@@ -0,0 +1,113 @@
+let items = [
+    {
+        "category": "pizzas",
+        "name": "Pizza Funghi",
+        "ingredients": "Tomatensoße, Salami, Frische Champignons",
+        "img": "pizza-funghi.jpg",
+        "price": 15.90,
+    }
+
+    {
+        "category": "pizzas",
+        "name": "Pizza Chorizp",
+        "ingredients": "Tomaten, Chorizo, Mozarella",
+        "img": "pizza-chorizo.jpg",
+        "price": 15.90,
+    }
+
+    {
+        "category": "pizzas",
+        "name": "Pizza Magherita",
+        "ingredients": "Tomatensoße, Mozarella",
+        "img": "pizza-magherita.jpg",
+        "price": 13.90,
+    }
+
+    {
+        "category": "pizzas",
+        "name": "Pizza Spicy",
+        "ingredients": "Tomaten, Paprika, Peperoni, Rote Zwiebeln, Cillisoße",
+        "img": "pizza-spicy.jpg",
+        "price": 15.90,
+    }
+
+    {
+        "category": "pizzas",
+        "name": "Pizza Quaddro Formaggi with Chicken",
+        "ingredients": "Chicken, Tomatensoße, Mozarella, Gorgonzola, Parmesan, Emmentaler",
+        "img": "pizza-quaddro.jpg",
+        "price": 14.90,
+    }
+
+    {
+        "category": "salads",
+        "name": "Rind Rukola Salat",
+        "ingredients": "Rind, Rukola, Tomaten, Parmesan, Balsamico Soße",
+        "img": "salat-warm-beef.jpg",
+        "price": 11.90,
+    }
+
+    {
+        "category": "salads",
+        "name": "Kleiner grüner Salat",
+        "ingredients": "Feldsalat, Gurken, Tomaten, Paprika, Karotten",
+        "img": "salat-mini-green.jpg",
+        "price": 8.90,
+    }
+
+    {
+        "category": "salads",
+        "name": "Meeresfrüchste Salat",
+        "ingredients": "Garnelen, Calamari, Tomaten, Gurken, Rucola, Zitronen-Dill Soße",
+        "img": "salat-sea-food.jpg",
+        "price": 11.90,
+    }
+
+    {
+        "category": "salads",
+        "name": "Tofu Salat",
+        "ingredients": "Grüner Salat, Tofu, Edamame, Karotten, Sesam-Ingwer Soße",
+        "img": "salat-green-tofu.jpg",
+        "price": 10.90,
+    }
+
+    {
+        "category": "drinks",
+        "name": "Cola",
+        "ingredients": "Coca Cola mit Eiswürfeln",
+        "img": "cola.jpg",
+        "price": 6.90,
+    }
+
+    {
+        "category": "drinks",
+        "name": "Fanta",
+        "ingredients": "Fanta mit Eiswürfeln",
+        "img": "fanta.jpg",
+        "price": 6.90,
+    }
+    
+    {
+        "category": "drinks",
+        "name": "Sprite",
+        "ingredients": "Sprite mit Eiswürfeln",
+        "img": "sprite.jpg",
+        "price": 6.90,
+    }
+
+    {
+        "category": "drinks",
+        "name": "Stilles Wasser",
+        "ingredients": "Stilles Wasser mit Eiswürfeln",
+        "img": "water.jpg",
+        "price": 6.90,
+    }
+
+    {
+        "category": "drinks",
+        "name": "Bier",
+        "ingredients": "Frisch gezapftes Bier",
+        "img": "beer.jpg",
+        "price": 7.90,
+    }
+]
