228da54f0e2153a5dc991b9ce59fdd82e96f0ba6
Alexander Ruhl <ruhl-alex@web.de>
2025-12-28 23:49:49 +0100

    fill items in template.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,49 +1,52 @@
-const ITEMS = [
+const PIZZAS = [
     {
-        "category": "pizzas",
         "name": "Pizza Funghi",
         "ingredients": "Tomatensoße, Salami, Frische Champignons",
         "img": "pizza-funghi.jpg",
+        "alt": "Pizza Funghi",
         "price": 15.90,
     },
 
     {
-        "category": "pizzas",
         "name": "Pizza Chorizp",
         "ingredients": "Tomaten, Chorizo, Mozarella",
         "img": "pizza-chorizo.jpg",
+        "alt": "Pizza Chorizo",
         "price": 15.90,
     },
 
     {
-        "category": "pizzas",
         "name": "Pizza Magherita",
         "ingredients": "Tomatensoße, Mozarella",
         "img": "pizza-magherita.jpg",
+        "alt": "Pizza Magherita",
         "price": 13.90,
     },
 
     {
-        "category": "pizzas",
         "name": "Pizza Spicy",
         "ingredients": "Tomaten, Paprika, Peperoni, Rote Zwiebeln, Cillisoße",
         "img": "pizza-spicy.jpg",
+        "alt": "Pizza Spicy",
         "price": 15.90,
     },
 
     {
-        "category": "pizzas",
         "name": "Pizza Quaddro Formaggi with Chicken",
         "ingredients": "Chicken, Tomatensoße, Mozarella, Gorgonzola, Parmesan, Emmentaler",
         "img": "pizza-quaddro.jpg",
+        "alt": "Pizza Quaddro Formaggi with Chicken",
         "price": 14.90,
-    },
+    }
+]
 
-    {
+const SALADS = [
+{
         "category": "salads",
         "name": "Rind Rukola Salat",
         "ingredients": "Rind, Rukola, Tomaten, Parmesan, Balsamico Soße",
         "img": "salat-warm-beef.jpg",
+        "alt": "Rind Rukola Salat",
         "price": 11.90,
     },
 
@@ -52,6 +55,7 @@ const ITEMS = [
         "name": "Kleiner grüner Salat",
         "ingredients": "Feldsalat, Gurken, Tomaten, Paprika, Karotten",
         "img": "salat-mini-green.jpg",
+        "alt": "Kleiner grüner Salat",
         "price": 8.90,
     },
 
@@ -60,6 +64,7 @@ const ITEMS = [
         "name": "Meeresfrüchste Salat",
         "ingredients": "Garnelen, Calamari, Tomaten, Gurken, Rucola, Zitronen-Dill Soße",
         "img": "salat-sea-food.jpg",
+        "alt": "Meeresfrüchste Salat",
         "price": 11.90,
     },
 
@@ -68,51 +73,54 @@ const ITEMS = [
         "name": "Tofu Salat",
         "ingredients": "Grüner Salat, Tofu, Edamame, Karotten, Sesam-Ingwer Soße",
         "img": "salat-green-tofu.jpg",
-        "price": 10.90,
-    },
+        "alt": "Tofu Salat",
+        "price": 10.90
+    }
+]
 
-    {
-        "category": "drinks",
+const DRINKS = [
+{
         "name": "Cola",
         "ingredients": "Coca Cola mit Eiswürfeln",
         "img": "cola.jpg",
+        "alt": "Cola",
         "price": 6.90,
     },
 
     {
-        "category": "drinks",
         "name": "Fanta",
         "ingredients": "Fanta mit Eiswürfeln",
         "img": "fanta.jpg",
+        "alt": "Fanta",
         "price": 6.90,
     },
     
     {
-        "category": "drinks",
         "name": "Sprite",
         "ingredients": "Sprite mit Eiswürfeln",
         "img": "sprite.jpg",
+        "alt": "Sprite",
         "price": 6.90,
     },
 
     {
-        "category": "drinks",
         "name": "Stilles Wasser",
         "ingredients": "Stilles Wasser mit Eiswürfeln",
         "img": "water.jpg",
+        "alt": "Stilles Wasser",
         "price": 6.90,
     },
 
     {
-        "category": "drinks",
         "name": "Bier",
         "ingredients": "Frisch gezapftes Bier",
         "img": "beer.jpg",
+        "alt": "Bier",
         "price": 7.90,
     }
 ]
 
-let basket = [
+let BASKET = [
     {
         "item": "",
         "price": 0,
---
+++ scripts/template.js
@@ -0,0 +1,65 @@
+function render() {
+    let contentRefPizzas = document.getElementById("item-container-pizzas");
+    contentRefPizzas.innerHTML = "";
+    let contentRefSalads = document.getElementById("item-container-salads");
+    contentRefSalads.innerHTML = "";
+    let contentRefDrinks = document.getElementById("item-container-drinks");
+    contentRefDrinks.innerHTML = "";
+    for (let indexPizza = 0; indexPizza < PIZZAS.length; indexPizza++) {
+        contentRefPizzas.innerHTML += getPizzaTemplate(indexPizza);
+    }
+    for (let indexSalad = 0; indexSalad < SALADS.length; indexSalad++) {
+        contentRefSalads.innerHTML += getSaladTemplate(indexSalad);
+    }
+    for (let indexDrink = 0; indexDrink < DRINKS.length; indexDrink++) {
+        contentRefDrinks.innerHTML += getDrinkTemplate(indexDrink);
+    }
+}
+
+function getPizzaTemplate(indexPizza) {
+        return `
+        <div class="item-container">
+            <img src="./assets/img/${PIZZAS[indexPizza].img}" alt="${PIZZAS[indexPizza].alt}" class="item-img">
+            <div class="item-info">
+                <span class="item-title">${PIZZAS[indexPizza].name}</span>
+                <span>${PIZZAS[indexPizza].ingredients}</span>
+            </div>
+            <div class="item-price-info">
+                <span class="item-price">${PIZZAS[indexPizza].price.toFixed(2)} €</span>
+                <div><button class="item-buy-button">Hinzufügen</button></div>
+            </div>
+        </div>
+        `;
+    }
+
+function getSaladTemplate(indexSalad) {
+        return `
+        <div class="item-container">
+            <img src="./assets/img/${SALADS[indexSalad].img}" alt="${SALADS[indexSalad].alt}" class="item-img">
+            <div class="item-info">
+                <span class="item-title">${SALADS[indexSalad].name}</span>
+                <span>${SALADS[indexSalad].ingredients}</span>
+            </div>
+            <div class="item-price-info">
+                <span class="item-price">${SALADS[indexSalad].price.toFixed(2)} €</span>
+                <div><button class="item-buy-button">Hinzufügen</button></div>
+            </div>
+        </div>
+        `;
+    }
+
+    function getDrinkTemplate(indexDrink) {
+        return `
+        <div class="item-container">
+            <img src="./assets/img/${DRINKS[indexDrink].img}" alt="${DRINKS[indexDrink].alt}" class="item-img">
+            <div class="item-info">
+                <span class="item-title">${DRINKS[indexDrink].name}</span>
+                <span>${DRINKS[indexDrink].ingredients}</span>
+            </div>
+            <div class="item-price-info">
+                <span class="item-price">${DRINKS[indexDrink].price.toFixed(2)} €</span>
+                <div><button class="item-buy-button">Hinzufügen</button></div>
+            </div>
+        </div>
+        `;
+    }
